d36855d359baf87ed4479484609bf8e8b7f5c081
Mohamed Youssef <mohamed.ahmed.abdel.monem@gmail.com>
2020-01-06 23:24:36 +0400

    post CRUD operation with joi validation

--- part2_express/api/posts.js
+++ part2_express/api/posts.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Joi = require("@hapi/joi");
 const posts = [
   {
     id: 1,
@@ -9,9 +10,24 @@ const posts = [
     id: 2,
     title: "second post",
     body: "moow post"
+  },
+  {
+    id: 3,
+    title: "third post",
+    body: "hhhh post"
   }
 ];
-const postsKeys = ["title", "body"];
+const postsSchema = Joi.object({
+  id: Joi.number().integer(),
+  title: Joi.string()
+    .min(5)
+    .max(40)
+    .required(),
+  body: Joi.string()
+    .min(5)
+    .max(255)
+    .required()
+});
 
 router.get("/", (req, res) => {
   const query = req.query;
@@ -21,23 +37,42 @@ router.get("/", (req, res) => {
   res.json(response);
 });
 
-router.post("/", (req, res) => {
-  const post = req.body;
-  const error = {};
-  for (let key of postsKeys) {
-    if (!post[key]) error[key] = `${key} is missing`;
-  }
-  if (Object.keys(error).length > 0) return res.status(400).json(error);
-  post.id = posts[posts.length - 1].id + 1;
-  posts.push(post);
-  res.status(201).json(post);
-});
-
 router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const post = posts.find(current => current.id == id);
+  const id = parseInt(req.params.id);
+  const post = posts.find(current => current.id === id);
   if (post) return res.json(post);
   res.status(404).send(`Post with id: ${id} was not found`);
+});
+
+router.post("/", (req, res) => {
+  const post = req.body;
+  const { error, value } = postsSchema.validate(post);
+
+  if (error) return res.status(400).json(error);
+  value.id = posts[posts.length - 1].id + 1;
+  posts.push(value);
+  res.status(201).json(value);
+});
+
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const currentPostIndex = posts.findIndex(current => current.id === id);
+  const currentPost = posts[currentPostIndex];
+  if (!currentPost)
+    return res.status(404).send(`Post with id: ${id} was not found`);
+  const { error, value: post } = postsSchema.validate(req.body);
+  if (error) return res.status(400).json(error);
+  const final = { ...currentPost, ...post };
+  posts[currentPostIndex] = final;
+  res.json(final);
+});
+
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const currentPostIndex = posts.findIndex(current => current.id === id);
+  if (currentPostIndex === -1)
+    return res.status(404).send(`Post with id: ${id} was not found`);
+  res.json(posts.splice(currentPostIndex, 1));
 });
 
 module.exports = router;

d01a98ce0da692f4f8bfda0b76cb804c56c79b00
Mohamed Youssef <mohamed.ahmed.abdel.monem@gmail.com>
2020-01-07 02:01:36 +0400

    adding express middleware helmet and morgan

--- part2_express/index.js
+++ part2_express/index.js
@@ -1,8 +1,5 @@
 const app = require("./server");
 const logger = require("../part1/logger");
 const env = require("./env");
-const api = require("./api");
-
-app.use(api);
 
 app.listen(env.port, () => logger(`listening to port ${env.port}`));
--- part2_express/server.js
+++ part2_express/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
-const logger = require("../part1/logger");
+const api = require("./api");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rfs = require("rotating-file-stream");
+const path = require("path");
 
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotate daily
+  path: path.join(__dirname, "log")
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  logger(`request url: ${req.url}, request method: ${req.method}`);
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
+app.use(api);
 
 app.get("/", (req, res) => {
   res.send("hello world");

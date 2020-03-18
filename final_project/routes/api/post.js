102a501d9603607e6130eb554e4f5462257f9cc7
Mohamed Youssef <mohamed.ahmed.abdel.monem@gmail.com>
2020-03-19 01:38:31 +0200

    adding readme file, apidoc to generate api documentations

--- final_project/routes/api/post.js
+++ final_project/routes/api/post.js
@@ -1,6 +1,26 @@
 const router = require("express").Router();
 const postRepo = require("../../repositories/post");
 
+/**
+ * @api {get} /api/posts Request all posts
+ * @apiVersion 0.1.0
+ * @apiPermission none
+ * @apiName GetPosts
+ * @apiGroup Posts
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [{
+ *       "title": "Title",
+ *       "body": "Body",
+ *       "auther": {
+ *          "firstname" : "John",
+ *          "lastname" : "Doa"
+ *        },
+ *       "isPublished" : false,
+ *       "tags": ["angular", "nodejs"]
+ *     }]
+ */
 router.get("/", async (req, res) => {
   try {
     const posts = await postRepo.getAll();
@@ -10,6 +30,46 @@ router.get("/", async (req, res) => {
   }
 });
 
+/**
+ * @api {get} /api/posts/:id Request one post
+ * @apiVersion 0.1.0
+ * @apiPermission none
+ * @apiName GetPost
+ * @apiGroup Posts
+ * @apiExample {curl} Example usage:
+ *     curl -i http://localhost:4300/api/posts/1
+ *
+ * @apiParam {String} id Posts unique ID.
+ *
+ * @apiSuccess {String} title Title of the Post.
+ * @apiSuccess {String} body  Body of the Post.
+ * @apiSuccess {Object} auther  Author of the Post.
+ * @apiSuccess {Boolean} isPublished  Publish state of the Post.
+ * @apiSuccess {String[]} tags  Tags of the Post.
+ * @apiSuccess {Object} comments  Comments of the Post.
+ * @apiSuccess {Object} meta  Meta of the Post.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "title": "Title",
+ *       "body": "Body",
+ *       "auther": {
+ *          "firstname" : "John",
+ *          "lastname" : "Doa"
+ *        },
+ *       "isPublished" : false,
+ *       "tags": ["angular", "nodejs"]
+ *     }
+ *
+ * @apiError PostNotFound The id of the Post was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "PostNotFound"
+ *     }
+ */
 router.get("/:id", async (req, res) => {
   try {
     const id = req.params.id;
